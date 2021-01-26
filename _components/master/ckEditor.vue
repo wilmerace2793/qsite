ea78fbd50542ad7a0c835c8661f34e8c8568af86
msolanogithub <msolano2505@gmail.com>
2021-01-26 10:10:20 -0500

    +Fix: qsite | CKEditor

--- _components/master/ckEditor.vue
+++ _components/master/ckEditor.vue
@@ -4,48 +4,48 @@
   </div>
 </template>
 <script>
-  //Components
-  import CKEditor from 'ckeditor4-vue';
+//Components
+import CKEditor from 'ckeditor4-vue';
 
-  export default {
-    props: {
-      value: {default: ''}
+export default {
+  props: {
+    value: {default: ''}
+  },
+  components: {ckEditor: CKEditor.component},
+  watch: {
+    value(newValue, oldValue) {
+      if (JSON.stringify(newValue) != JSON.stringify(oldValue))
+        this.responseValue = this.$clone(newValue)
     },
-    components: {ckEditor: CKEditor.component},
-    watch: {
-      value(newValue, oldValue) {
-        if (JSON.stringify(newValue) != JSON.stringify(oldValue))
-          this.responseValue = this.$clone(newValue)
-      },
-      responseValue(newValue, oldValue) {
-        if (JSON.stringify(newValue) != JSON.stringify(oldValue))
-          this.$emit('input', this.$clone(newValue))
+    responseValue(newValue, oldValue) {
+      if (JSON.stringify(newValue) != JSON.stringify(oldValue))
+        this.$emit('input', this.$clone(newValue))
 
-      }
-    },
-    mounted() {
-      this.$nextTick(function () {
-        this.init()
-      })
-    },
-    data() {
-      return {
-        responseValue: '',
-        configEditor: {
-          filebrowserBrowseUrl: this.$router.resolve({name: 'app.media.select'}).href,
-          extraPlugins: 'embed,autoembed,colorbutton,colordialog',
-          embed_provider: '//iframe.ly/api/oembed?url={url}&callback={callback}&api_key=7e0aa12b0cd2c01651346b',
-          allowedContent: 'iframe[*]'
-        }
-      }
-    },
-    computed: {},
-    methods: {
-      init() {
-        this.responseValue = this.$clone(this.value)
+    }
+  },
+  mounted() {
+    this.$nextTick(function () {
+      this.init()
+    })
+  },
+  data() {
+    return {
+      responseValue: '',
+      configEditor: {
+        allowedContent: true,
+        filebrowserBrowseUrl: this.$router.resolve({name: 'app.media.select'}).href,
+        extraPlugins: 'embed,autoembed,colorbutton,colordialog',
+        embed_provider: '//iframe.ly/api/oembed?url={url}&callback={callback}&api_key=7e0aa12b0cd2c01651346b'
       }
     }
+  },
+  computed: {},
+  methods: {
+    init() {
+      this.responseValue = this.$clone(this.value)
+    }
   }
+}
 </script>
 <style lang="stylus">
 </style>
