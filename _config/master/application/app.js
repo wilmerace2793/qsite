7d7cdf19c90c74e1f26058ff1a71eda30aad6fe7
T-JULIAND <juliandavidbonillatorres@gmail.com>
2021-05-07 20:27:00 -0500

    +Update: Iadmin | Version 2.3.45

--- _config/master/application/app.js
+++ _config/master/application/app.js
@@ -5,7 +5,7 @@ tagsToParceHost.forEach(tagToReplace => host = host.replace(tagToReplace, ''))
 let loadFrom = host.split('/')[1]
 
 export default {
-  version: '2.3.44',
+  version: '2.3.45',
   mode: (loadFrom && (['iadmin', 'ipanel'].indexOf(loadFrom) != -1)) ? loadFrom : 'iadmin',//Define load mode
   validateModeAccess: true,//Define if validate app mode access
   forceRoleAndDepartment: false,//Force to select role and department
