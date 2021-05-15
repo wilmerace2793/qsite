4028d8c6d00c751e2d0715e20941e3e0b6846981
T-JULIAND <juliandavidbonillatorres@gmail.com>
2021-05-14 19:36:17 -0500

    +Update: qsite | Iadmin v2.3.48

--- _config/master/application/app.js
+++ _config/master/application/app.js
@@ -5,7 +5,7 @@ tagsToParceHost.forEach(tagToReplace => host = host.replace(tagToReplace, ''))
 let loadFrom = host.split('/')[1]
 
 export default {
-  version: '2.3.47',
+  version: '2.3.48',
   mode: (loadFrom && (['iadmin', 'ipanel'].indexOf(loadFrom) != -1)) ? loadFrom : 'iadmin',//Define load mode
   validateModeAccess: true,//Define if validate app mode access
   forceRoleAndDepartment: false,//Force to select role and department
