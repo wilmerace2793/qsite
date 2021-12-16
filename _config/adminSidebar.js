ac0f29de9f3125f4a78c383329d826e6f3b8b16c
msolanogithub <msolano2505@gmail.com>
2021-12-16 17:59:02 -0500

    +Update: Translation to group sidebar

--- _config/adminSidebar.js
+++ _config/adminSidebar.js
@@ -3,8 +3,8 @@ const pages = config('pages') // Get Pages from config
 //Places
 export default [
   {
-    title: 'qsite.sidebar.adminGroupOrganizations',
-    icon: 'fas fa-chess',
+    title: 'qsite.sidebar.adminGroup',
+    icon: 'fas fa-chess-rook',
     children: [
       pages.qsite.organizations,//organizations
       pages.qsite.categories//organization categories
