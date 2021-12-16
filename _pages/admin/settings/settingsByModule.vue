41606e8cf1cb48b5c5a2811060ed917acf1fbfea
msolanogithub <msolano2505@gmail.com>
2021-12-16 18:01:13 -0500

    +Add: Page to settings by module

---
+++ _pages/admin/settings/settingsByModule.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="pageId">
+    Perzonalization page...
+  </div>
+</template>
+<script>
+export default {
+  props: {},
+  components: {},
+  watch: {},
+  mounted() {
+    this.$nextTick(function () {
+      this.init()
+    })
+  },
+  data() {
+    return {
+      loading: false,
+      data: []
+    }
+  },
+  computed: {},
+  methods: {
+    init() {
+    }
+  }
+}
+</script>
+<style lang="stylus">
+</style>
