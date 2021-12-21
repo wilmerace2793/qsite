adc394f26d94ada5668fb4efd2afbf0c7bddc3c8
msolanogithub <msolano2505@gmail.com>
2021-12-21 08:31:27 -0500

    +Add: moduleName attribute to mainConfig

--- _config/main.js
+++ _config/main.js
@@ -1,4 +1,7 @@
 export default {
+  moduleName: 'isite',
+  modulesSettings: ['core', 'isite', 'icustom'],
+  moduleTitle: 'qsite.sidebar.adminGroup',
   //Entities
   entityNames: {
     recommendation: 'recommendation',
