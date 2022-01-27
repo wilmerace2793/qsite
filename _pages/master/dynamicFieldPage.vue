dfb585a8d9366ea19f41108a82795a8b874021d4
Michael <msolano@imaginacolombia.com>
2022-01-27 16:17:04 -0500

    +Update: dynamicFieldPage

--- _pages/master/dynamicFieldPage.vue
+++ _pages/master/dynamicFieldPage.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="testPageId">
-    <dynamic-field v-for="(field, keyField) in fieldsConfig" :key="keyField"
-                   :field="{...field, ...helpProp}" class="q-mb-md"/>
+    <div class="row">
+      <div v-for="(field, keyField) in fieldsConfig" :key="keyField" class="col-12">
+        <dynamic-field class="q-mb-md" :field="field"/>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -20,16 +23,15 @@ export default {
     }
   },
   computed: {
-    helpProp(){
-      return {
-        help : {
-          description : 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad amet aspernatur atque, error harum ipsa magni odit recusandae, repellat totam vitae? Impedit inventore necessitatibus reiciendis soluta! Deserunt, harum, sunt.'
+    fieldsConfig() {
+      let globalData = {
+        help: {
+          description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium ad amet aspernatur atque, error harum ipsa magni odit recusandae, repellat totam vitae? Impedit inventore necessitatibus reiciendis soluta! Deserunt, harum, sunt.'
         }
       }
-    },
-    fieldsConfig() {
       return {
         crud: {
+          ...globalData,
           value: null,
           type: 'crud',
           props: {
@@ -46,42 +48,52 @@ export default {
           },
         },
         input: {
+          ...globalData,
           type: 'input',
           props: {label: 'type: input'}
         },
         search: {
+          ...globalData,
           type: 'search',
           props: {label: 'type: search'}
         },
         date: {
+          ...globalData,
           type: 'date',
           props: {label: 'type: date'}
         },
         hour: {
+          ...globalData,
           type: 'hour',
           props: {label: 'type: hour'}
         },
         fullDate: {
+          ...globalData,
           type: 'fullDate',
           props: {label: 'type: fullDate'}
         },
         select: {
+          ...globalData,
           type: 'select',
-          props: {label: 'type: select', options : []}
+          props: {label: 'type: select', options: []}
         },
         treeSelect: {
+          ...globalData,
           type: 'treeSelect',
           props: {label: 'type: treeSelect'}
         },
         html: {
+          ...globalData,
           type: 'html',
           props: {label: 'type: html'}
         },
         checkbox: {
+          ...globalData,
           type: 'checkbox',
           props: {label: 'type: checkbox'}
         },
         media: {
+          ...globalData,
           value: {},
           type: 'media',
           props: {
@@ -92,41 +104,49 @@ export default {
           }
         },
         inputColor: {
+          ...globalData,
           type: 'inputColor',
           props: {label: 'type: inputColor'}
         },
         toggle: {
+          ...globalData,
           type: 'toggle',
           props: {label: 'type: toggle'}
         },
         signature: {
+          ...globalData,
           type: 'signature',
           props: {label: 'type: signature'}
         },
         rating: {
+          ...globalData,
           type: 'rating',
           props: {label: 'type: rating'}
         },
         selectIcon: {
+          ...globalData,
           type: 'selectIcon',
           props: {label: 'type: selectIcon'}
         },
         optionGroup: {
+          ...globalData,
           type: 'optionGroup',
           props: {
             label: 'type: optionGroup',
-            options : [
-              {label : 'opt1', value : 'opt1'},
-              {label : 'opt2', value : 'opt2'},
-              {label : 'opt3', value : 'opt3'}
+            options: [
+              {label: 'opt1', value: 'opt1'},
+              {label: 'opt2', value: 'opt2'},
+              {label: 'opt3', value: 'opt3'}
             ]
           }
         },
         schedulable: {
+          ...globalData,
           type: 'schedulable',
           props: {label: 'type: schedulable'}
         },
         json: {
+          ...globalData,
           type: 'json',
           props: {label: 'type: json'}
         },
