355e3f5387c0d4c127d237e3c7c3afe4382c71cb
msolano <msolano@imaginacolombia.com>
2022-03-09 13:13:40 -0500

    +Add: Main page index

---
+++ _pages/main/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="mainPage">
+    <!--index admin-->
+    <index-admin v-if="appMode == 'iadmin'"/>
+    <!--index panel-->
+    <index-panel v-else/>
+  </div>
+</template>
+<script>
+//components
+import indexAdmin from '@imagina/qsite/_pages/admin/index'
+import indexPanel from '@imagina/qsite/_pages/panel/index'
+
+export default {
+  props: {},
+  components: {indexAdmin, indexPanel},
+  watch: {},
+  mounted() {
+    this.$nextTick(function () {
+    })
+  },
+  data() {
+    return {
+      appMode: config('app.mode')
+    }
+  },
+  computed: {},
+  methods: {}
+}
+</script>
+<style lang="stylus">
+</style>
