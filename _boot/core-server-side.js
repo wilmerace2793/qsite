e6635da01e6790cf1951b1da1d21c43797c1a13c
msolano <msolano@imaginacolombia.com>
2022-03-10 10:20:48 -0500

    +Update: menu class to load sidebar

--- _boot/core-server-side.js
+++ _boot/core-server-side.js
@@ -1,4 +1,6 @@
 import AutoLoadRoutes from '@imagina/qsite/_config/master/application/routes'
+import lodash from 'lodash'
+
 export default async ({app, router, store, Vue}) => {
   await Promise.all([
     store.dispatch('qsiteApp/GET_PAGES'),
@@ -8,5 +10,5 @@ export default async ({app, router, store, Vue}) => {
     store.dispatch('qsiteApp/GET_SITE_HOOKS'),
   ])
   //add all route from backend in router method later resolve all promise
-  router.addRoutes(AutoLoadRoutes.getRoutes(store.state.qsiteApp.pages || []))
+  router.addRoutes(AutoLoadRoutes.getRoutes(lodash.cloneDeep(store.state.qsiteApp.pages || [])))
 }
