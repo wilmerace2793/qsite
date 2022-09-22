cc1966ffa3f867cd3a5c5a27c3b236704d6a1908
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2022-09-22 12:09:06 -0500

    add validation rutes

--- _layouts/blank.vue
+++ _layouts/blank.vue
@@ -8,15 +8,18 @@
 </template>
 
 <script>
+import { computed } from 'vue'
+
   export default {
     meta () {
-      let routetitle = ((this.$route.meta && this.$route.meta.title) ? this.$route.meta.title : '')
+      let routeTitle = ((this.$route.meta && this.$route.meta.title) ? this.$route.meta.title : '')
+      if (this.$route.meta && this.$route.meta.headerTitle) routeTitle = this.$route.meta.headerTitle
       let siteName = this.$store.getters['qsiteApp/getSettingValueByName']('core::site-name')
       let siteDescription = this.$store.getters['qsiteApp/getSettingValueByName']('core::site-description')
       let iconHref = this.$store.getters['qsiteApp/getSettingMediaByName']('isite::favicon').path
 
       return {
-        title: `${routetitle} | ${siteName}`,
+        title: `${this.useLegacyStructure ? this.$tr(routeTitle) : routeTitle} | ${siteName}`,
         meta : {
           description: { name: 'description', content: (siteDescription || siteName) },
         },
@@ -24,6 +27,13 @@
       }
     },
     async mounted () {
+    },
+    computed: {
+      useLegacyStructure() {
+      const legacyStructure = parseInt(this.$store.getters["qsiteApp/getSettingValueByName"]("isite::legacyStructureCMS") || 0)
+      return legacyStructure === 1 || false
+
+    },
     }
   }
 </script>
