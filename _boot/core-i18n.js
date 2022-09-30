e231e7a9d2f61f93e633136522ca038b717b45c2
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2022-09-30 17:14:54 -0500

    add new helper date

--- _boot/core-i18n.js
+++ _boot/core-i18n.js
@@ -70,4 +70,9 @@ export default async ({ router, app, Vue, store, ssrContext }) => {
     //Repsonse
     return app.i18n.d(moment(date, 'YYYY-MM-DD HH:mm:ss').toDate(), params.type)
   }
+  //Date translate
+  Vue.prototype.$trdT = (date) => {
+    //Transform date from UTC
+    return moment(date).format('MMMM, DD, YYYY HH:mm');
+  }
 }
