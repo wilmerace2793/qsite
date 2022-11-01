1598fcfe8a42e7f47ea3c5c1bcd44379880f0906
msolano <msolano@imaginacolombia.com>
2022-11-01 12:11:22 -0500

    +Update: ignore the field to convert to snakeCase when submit the create and update service

--- _plugins/helper.js
+++ _plugins/helper.js
@@ -201,19 +201,17 @@ class Helper {
   }
 
   //Convert object keys to snake_case
-  toSnakeCase(object) {
+  toSnakeCase(object, params = {}) {
+    //Items to ignore when try to convert to snakeCase
+    const notToSnakeCase = [...(params.notToSnakeCase || []), ...["options", "fields", "settings", "permissions"]]
     //function recursive to loop all items from object
     let convertObject = (dataObject) => {
       let response = {}//Object to save fields vonverted
       //Loop all items for convert
 
       for (var item in dataObject) {
-
         let itemValue = dataObject[item]//Value from item
-        if (item !== 'options'
-          && item !== 'fields'
-          && item !== 'settings'
-          && item !== 'permissions') {
+        if (!notToSnakeCase.includes(item)) {
           //If value is object, also convert value
           if ((typeof itemValue === 'object' && itemValue !== null)) {
             if (Array.isArray(itemValue)) {
