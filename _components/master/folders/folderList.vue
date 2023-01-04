af166bac2bd1567430a254bf5f7a010baac93326
Wilmer Cristancho <wilmerc@imaginacolombia.com>
2023-01-04 09:12:56 -0500

    formater code

--- _components/master/folders/folderList.vue
+++ _components/master/folders/folderList.vue
@@ -11,42 +11,46 @@
         >
           <template v-slot:header>
             <q-item-section avatar>
-              <q-icon name="folder" color="primary" size="2rem"/>
+              <q-icon name="folder" color="primary" size="2rem" />
             </q-item-section>
-            <q-item-section class="folder-title-drag tw-py-5 f-cursor-grab" @mouseover="showCollapse">
-              <q-item-label class="tw-text-xl tw-font-bold" lines="1">{{ folder.title || folder.name || folder.id }}</q-item-label>
+            <q-item-section
+              class="folder-title-drag tw-py-5 f-cursor-grab"
+              @mouseover="showCollapse"
+            >
+              <q-item-label class="tw-text-xl tw-font-bold" lines="1">
+                {{ folder.title || folder.name || folder.id }}
+              </q-item-label>
             </q-item-section>
             <q-item-section side>
               <div class="row items-center">
                 <btn-menu
-                      :actions="fieldActions(folder)"
-                      :action-data="folder"
-                    />
+                  :actions="fieldActions(folder)"
+                  :action-data="folder"
+                />
               </div>
             </q-item-section>
           </template>
-          <q-list 
-            separator 
+          <q-list
+            separator
             class="tw-pt-2 tw-rounded-b-xl"
-            v-show="!folder.loading"
+            v-show="folder.isCollapse && !folder.loading"
           >
-             <reportList 
-                :folder="folder"
-              />
+            <reportList :folder="folder" />
           </q-list>
           <div
-            v-show="folder.loading"
+            v-show="folder.isCollapse && folder.loading"
             class="
               tw-bg-gray-100
               tw-h-32
-              tw-flex 
+              tw-flex
               tw-absolute
-              tw-inset-0 
-              tw-justify-center 
+              tw-inset-0
+              tw-justify-center
               tw-py-8
-              tw-rounded-b-md"
-            >
-              <q-spinner color="primary" size="3em" />
+              tw-rounded-b-md
+            "
+          >
+            <q-spinner color="primary" size="3em" />
           </div>
         </q-expansion-item>
       </q-card-section>
@@ -55,42 +59,48 @@
 </template>
 
 <script>
-import draggable from "vuedraggable"
-import reportList from './reportList';
+import draggable from "vuedraggable";
+import reportList from "./reportList";
 import foldersStore from "./store/foldersStore.js";
 export default {
-components: {
-  reportList,
-},
-props: {
-  folder: {
-    type: Object,
-    required: true,
+  components: {
+    reportList,
   },
-},
-inject: [
-  'getRelationData', 
-  'fieldActions',
-],
-computed: {
-  dragReports() {
-    return foldersStore().getDragReports();
+  props: {
+    folder: {
+      type: Object,
+      required: true,
+    },
   },
-},
-methods: {
-  async showCollapse() {
-     if(this.dragReports) {
-      await this.getRelation();
-      this.folder.isCollapse = true;
-    };
+  inject: {
+    getRelationData: {
+      type: Function,
+      default: () => false,
+    },
+    fieldActions: {
+      type: Function,
+      default: () => false,
+    },
   },
-  async getRelation() {
-    if(this.folder.reportList.length === 0) {
-      await this.getRelationData(this.folder);
-    };
+  computed: {
+    dragReports() {
+      return foldersStore().getDragReports();
+    },
   },
-}
-}
+  methods: {
+    async showCollapse() {
+      if (this.dragReports) {
+        await this.getRelation();
+        this.folder.isCollapse = true;
+      }
+    },
+    async getRelation() {
+      if (this.folder.reportList.length === 0) {
+        await this.getRelationData(this.folder);
+      }
+    },
+  },
+};
 </script>
 <style>
 .f-cursor-grab {
