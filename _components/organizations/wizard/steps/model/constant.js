18b9e5c632266c646e2402f10810a29b8ecac8bf
José Alejandro Sabogal Loaiza <jose.alejandro.sabogal.loaiza@gmail.com>
2023-05-25 07:19:17 -0500

    rename folder model

---
+++ _components/organizations/wizard/steps/model/constant.js
@@ -0,0 +1,16 @@
+export const STEP_REGISTER = 1;
+export const STEP_TERMS = 2;
+export const STEP_COMPANY = 3;
+export const STEP_CATEGORIES = 4;
+export const STEP_THEMES = 6;
+export const STEP_PLANS = 5;
+export const THEME_BASE_ID = 15; // id de las plantillas en general ecommerce
+export const PLAN_BASE_ID = 25; // id de categoria que tiene los planes
+export const STEP_NAME_REGISTER = 'admin_organization_wizard_register';
+export const STEP_NAME_TERMS = 'admin_organization_wizard_terms';
+export const STEP_NAME_COMPANY = 'admin_organization_wizard_organization';
+export const STEP_NAME_CATEGORIES = 'admin_organization_wizard_category';
+export const STEP_NAME_THEMES = 'admin_organization_wizard_theme';
+export const STEP_NAME_PLANS = 'admin_organization_wizard_plan';
+export const LIMIT_PAGE = 30;
+
