64acd9474f6a739b3a547e2dab0dce6d31ace34a
Wilmerace <wilmerc@imaginacolombia.com>
2023-07-10 16:45:50 -0500

    Delete: spinner

--- _boot/core-axios.js
+++ _boot/core-axios.js
@@ -1,10 +1,10 @@
 import axios from 'axios'
 import alert from '@imagina/qsite/_plugins/alert'
-import {Loading, QSpinnerCube} from 'quasar'
+import {Loading} from 'quasar'
 
 export default function ({app, router, store, Vue, ssrContext}) {
   //Open Loading
-  Loading.show({spinner : QSpinnerCube, spinnerColor : "cyan"})
+  Loading.show()
   //=========== Set base url to axios
   let baseUrl = config('app.baseUrl')
   let tagsToParceHost = ['http://', 'https://', ':8080', ':3000', 'www.']
