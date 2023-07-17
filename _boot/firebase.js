5f59bb0a51dbf9f80a831e3710b00c48de069afa
Wilmerace <wilmerc@imaginacolombia.com>
2023-07-17 17:08:38 -0500

    Firebase

--- _boot/firebase.js
+++ _boot/firebase.js
@@ -1,7 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { getMessaging, getToken } from "firebase/messaging";
 
-const firebaseConfig = {
+import storeFirebase from '@imagina/qnotification/_store/firebase/index.ts'
+
+
+export default function ({ app, router, store, Vue, ssrContext }) {
+  const firebaseConfig = {
     apiKey: "AIzaSyBKed8DEWi0Gk1-40_eas3Byr8Ln_WjEeQ",
     authDomain: "notifications-edc9c.firebaseapp.com",
     projectId: "notifications-edc9c",
@@ -9,29 +13,31 @@ const firebaseConfig = {
     messagingSenderId: "626515463422",
     appId: "1:626515463422:web:5d74016b36dcbe4b6629af",
     measurementId: "G-F7FC44SSCV"
-};
+  };
 
-function requestPermission() {
-  Notification.requestPermission().then((permission) => {
-    if (permission === "granted") {
-      console.log("Notification permission granted.");
-      const app = initializeApp(firebaseConfig);
+  function requestPermission() {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        console.log("Notification permission granted.");
+        const app = initializeApp(firebaseConfig);
 
-      const messaging = getMessaging(app);
-      getToken(messaging, {
-        vapidKey:
-          "BF3U4fgT2TJgtkCVWsCYN2RCYcY2X_PsxqiLlE9sHPPPvzAWr0XWmRfJcJh12fizpzTtakHZkJEAkbZx-m-zaFM",
-      }).then((currentToken) => {
-        if (currentToken) {
-          console.log("currentToken: ", currentToken);
-        } else {
-          console.log("Can not get token");
-        }
-      });
-    } else {
-      console.log("Do not have permission!");
-    }
-  });
+        const messaging = getMessaging(app);
+        getToken(messaging, {
+          vapidKey:
+            "BF3U4fgT2TJgtkCVWsCYN2RCYcY2X_PsxqiLlE9sHPPPvzAWr0XWmRfJcJh12fizpzTtakHZkJEAkbZx-m-zaFM",
+        }).then((currentToken) => {
+          if (currentToken) {
+            console.log("currentToken: ", currentToken);
+            storeFirebase.token = currentToken;
+          } else {
+            console.log("Can not get token");
+          }
+        });
+      } else {
+        console.log("Do not have permission!");
+      }
+    });
+  }
+
+  requestPermission();
 }
-
-requestPermission();
