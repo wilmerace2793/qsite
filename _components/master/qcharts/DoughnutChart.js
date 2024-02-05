f1fb00bf7e690e296716aa458d87d3f059cf4007
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-02-05 10:49:13 -0500

    :Temporary solution for the error

--- _components/master/qcharts/DoughnutChart.js
+++ _components/master/qcharts/DoughnutChart.js
@@ -1,9 +1,22 @@
-import { Doughnut, mixins } from 'vue-chartjs'
-const { reactiveProp } = mixins
+// [ptc] - 2021-03-04 - 16:00:00
+// import { Doughnut, mixins } from 'vue-chartjs'
+// const { reactiveProp } = mixins
+
+//[ptc]
+// export default {
+//   extends: Doughnut,
+//   mixins: [reactiveProp],
+//   props: ['options'],
+//   mounted () {
+//     // this.chartData is created in the mixin.
+//     // If you want to pass options please create a local options object
+//     this.renderChart(this.chartData, this.options)
+//   }
+// }
 
 export default {
-  extends: Doughnut,
-  mixins: [reactiveProp],
+  extends: {},
+  mixins: [],
   props: ['options'],
   mounted () {
     // this.chartData is created in the mixin.
