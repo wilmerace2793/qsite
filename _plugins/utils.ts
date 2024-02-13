0c5512af9ff54b68b018975e8e54d80954573660
Wilmer Cristancho <108033674+wilmerace2793@users.noreply.github.com>
2024-02-13 09:50:21 -0500

    utils.ts file is created (#276)

---
+++ _plugins/utils.ts
@@ -0,0 +1,34 @@
+import alert from 'modules/qsite/_plugins/alert.js'
+import array from 'modules/qsite/_plugins/array.js'
+import date from 'modules/qsite/_plugins/date.js'
+import helper from 'modules/qsite/_plugins/helper.js'
+import cache from 'modules/qsite/_plugins/cache.js'
+import {remember} from 'modules/qsite/_plugins/remember.js'
+import hook from 'modules/qsite/_plugins/hook.js'
+import eventBus from 'modules/qsite/_plugins/eventBus.js'
+import filter from 'modules/qsite/_plugins/filter.js'
+import cacheOffline from 'modules/qsite/_plugins/cacheOffline.js'
+import findValue from 'modules/qsite/_plugins/findValue.js'
+import lodash from 'lodash'
+import moment from 'moment'
+import {uid} from 'quasar'
+import {openURL} from 'quasar'
+
+export default {
+  alert,
+  array,
+  date,
+  helper,
+  cache,
+  remember,
+  hook,
+  eventBus,
+  filter,
+  cacheOffline,
+  findValue,
+  lodash,
+  moment,
+  uid,
+  openURL,
+}
+
