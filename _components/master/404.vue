7477f5910704e4cc24174712291ee9beeccd03a6
IMNicolasM <143624699+IMNicolasM@users.noreply.github.com>
2024-02-15 14:40:02 -0500

    Addeed: Pages in root project (#291)
    * Addeed: Pages in root project
    
    * Fixed: Move siteSettings to qsite
    
    * Fixed: Move siteSettings to qsite

--- _components/master/404.vue
+++ _components/master/404.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="notFoundComponent" class="text-center">
-    <q-icon name="power_off" color="grey-7" size="100px"/>
+    <q-icon name="fa-light fa-plug-circle-xmark" color="grey-7" size="100px"/>
     <p class="text-faded">{{labelText}}<strong>(404)</strong></p>
     <q-btn color="primary" style="width:200px;" @click="$router.push({name : routeName})"
            v-if="showButton" :label="buttonText" rounded unelevated/>
--- _config/mainPages.js
+++ _config/mainPages.js
@@ -1,58 +1,6 @@
 import appConfig from 'src/setup/app'
 
 export default {
-  //Home Page
-  home: {
-    permission: null,
-    activated: true,
-    path: '/',
-    name: 'app.home',
-    layout: () => import('layouts/master.vue'),
-    page: (appConfig.mode == 'iadmin') ? () => import('modules/qsite/_pages/admin/index.vue') :
-      () => import('modules/qsite/_pages/panel/index.vue'),
-    title: 'isite.cms.sidebar.pageHome',
-    icon: 'fal fa-home',
-    authenticated: true,
-    subHeader: {
-      refresh: true
-    }
-  },
-  //Not authorize
-  notAuthorized: {
-    permission: null,
-    activated: true,
-    path: '/not-authorized',
-    name: 'app.not.authorized',
-    layout: () => import('layouts/blank'),
-    page: () => import('modules/qsite/_pages/master/notAuthorized'),
-    title: 'isite.cms.sidebar.pageHome',
-    icon: 'fas fa-home',
-    authenticated: true,
-  },
-  //test PAge
-  testPage: {
-    permission: null,
-    activated: true,
-    path: '/test-page',
-    name: 'app.test.page',
-    layout: () => import('layouts/master'),
-    page: () => import('modules/qsite/_pages/master/testPage'),
-    title: 'isite.cms.sidebar.pageHome',
-    icon: 'fal fa-flask',
-    authenticated: true,
-  },
-  //Dynamic Fields Page
-  dynamicFieldsPage: {
-    permission: null,
-    activated: true,
-    path: '/dynamic-fields',
-    name: 'app.dynamic.fields.page',
-    layout: () => import('layouts/master'),
-    page: () => import('modules/qsite/_pages/master/dynamicFieldPage'),
-    title: 'isite.cms.sidebar.pageHome',
-    icon: 'fas fa-flask',
-    authenticated: true,
-  },
   //organization Wizard
   organizationWizard: {
     permission: null,
