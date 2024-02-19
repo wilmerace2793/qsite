5fbaa42e92b725e4af693b2b622d2892fb5f4c47
Wilmer Cristancho <108033674+wilmerace2793@users.noreply.github.com>
2024-02-19 14:11:32 -0500

    Chart js (#303)
    * update charJs
    
    * se ajusta graficas y se elimina archivos que no son necesarios

--- _components/master/charts.vue
+++ _components/master/charts.vue
@@ -2,87 +2,106 @@
   <div class="q-mt-sm box box-auto-hieght small">
     <!--charts Components -->
     <!-- line chart -->
-    <line-chart v-if="lineChart" :chart-data="data" :options="options" id="line"></line-chart>
+    <Line v-if="lineChart" :data="data" :options="options" id="line" />
     <!-- bar chart -->
-    <bar-chart v-if="barChart" :chart-data="data" :options="options" id="bar"></bar-chart>
+    <Bar v-if="barChart" :data="data" :options="options" id="bar" />
     <!-- pie chart -->
-    <pie-chart v-if="pieChart" :chart-data="data" :options="options" id="pie"></pie-chart>
+    <Pie v-if="pieChart" :data="data" :options="options" id="pie" />
     <!-- doughnut cart -->
-    <doughnut-chart v-if="doughnutChart" :chart-data="data" :options="options" id="doughnut"></doughnut-chart>
+    <Doughnut
+      v-if="doughnutChart"
+      :data="data"
+      :options="options"
+      id="doughnut"
+    />
   </div>
 </template>
 
 <script>
-  import LineChart from './qcharts/LineChart.js'
-  import BarChart from './qcharts/BarChart.js'
-  import PieChart from './qcharts/PieChart.js'
-  import DoughnutChart from './qcharts/DoughnutChart.js'
-  import Exporter from "vue-chartjs-exporter";
-  export default {
-    components: {
-      LineChart,
-      BarChart,
-      PieChart,
-      DoughnutChart
-    },
-    props: {
-      chartsData: {}
-    },
-    data () {
+import { Bar, Doughnut, Line, Pie } from 'vue-chartjs';
+import {
+  Chart as ChartJS,
+  Title,
+  Tooltip,
+  Legend,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+} from 'chart.js';
+import Exporter from 'vue-chartjs-exporter';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+export default {
+  components: {
+    Line,
+    Bar,
+    Pie,
+    Doughnut,
+  },
+  props: {
+    chartsData: {},
+  },
+  data() {
+    return {
+      lineChart: false,
+      barChart: false,
+      pieChart: false,
+      doughnutChart: false,
+      download_status: 'Download Charts',
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        locale: 'es-ES',
+      },
+    };
+  },
+  mounted() {
+    this.$nextTick(function () {
+      this.init();
+    });
+  },
+  computed: {
+    //reder chart
+    data() {
       return {
-        lineChart: false,
-        barChart: false,
-        pieChart: false,
-        doughnutChart: false,
-        download_status: "Download Charts",
-        options: {
-          responsive: true,
-          maintainAspectRatio: false,
-          locale: 'es-ES'
-        }
-      }
+        labels: this.chartsData.dataLabels.map((item) => item.name || item),
+        datasets: this.chartsData.dataSets,
+      };
     },
-    mounted () {
-      this.$nextTick(function () {
-        this.init()
-      })
-    }, 
-    computed: {
-      //reder chart
-      data () {
-        return {
-          labels: this.chartsData.dataLabels.map((item) => item.name || item),
-          datasets: this.chartsData.dataSets
-        }
-      }
+  },
+  methods: {
+    //init
+    init() {
+      this.selectChart();
     },
-    methods: {
-      //init
-      init () {
-        this.selectChart()
-      },
-      //select chart
-      selectChart () {
-        return this[this.chartsData.type] = true
-      },
-      //esport to pdf chart
-      exportToPDF() {
-        this.download_status = "Processing...";
-        let line = document.getElementById("line");
+    //select chart
+    selectChart() {
+      return (this[this.chartsData.type] = true);
+    },
+    //esport to pdf chart
+    exportToPDF() {
+      this.download_status = 'Processing...';
+      let line = document.getElementById('line');
 
-        const exp = new Exporter([line]);
-        exp.export_pdf().then((pdf) => {
-          pdf.save("charts.pdf");
-          this.download_status = "Download Charts";
-        });
-      },
+      const exp = new Exporter([line]);
+      exp.export_pdf().then((pdf) => {
+        pdf.save('charts.pdf');
+        this.download_status = 'Download Charts';
+      });
     },
-  }
+  },
+};
 </script>
 
 <style>
-  .small {
-    width: 100%;
-    height: 450px;
-  }
+.small {
+  width: 100%;
+  height: 450px;
+}
 </style>
