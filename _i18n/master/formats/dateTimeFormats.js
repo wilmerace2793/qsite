ec296e20397f26e396cf909945b10f4d8bd97176
SneiderDeveloper <37787288+SneiderDeveloper@users.noreply.github.com>
2024-04-29 16:52:44 -0500

    Update dateTimeFormats.js with new date and time formats

--- _i18n/master/formats/dateTimeFormats.js
+++ _i18n/master/formats/dateTimeFormats.js
@@ -1,30 +1,37 @@
 //Watch documentation of formats type here: http://www.ecma-international.org/ecma-402/2.0/#sec-intl-datetimeformat-constructor
 
+const en = {
+  day: {weekday: 'long',},
+  month: {month: 'long'},
+  year: {year: 'numeric'},
+  small: {
+    month: 'long', day: '2-digit'
+  },
+  short: {
+    year: 'numeric', month: 'long', day: '2-digit'
+  },
+  long: {
+    year: 'numeric', month: 'long', day: '2-digit',
+    hour: 'numeric', minute: 'numeric', hour12: true
+  },
+  explicit: {
+    weekday: 'long', year: 'numeric', month: 'long', day: '2-digit'
+  },
+  time: {
+    hour: '2-digit', minute: 'numeric', hour12: true
+  },
+  shortHuman: {
+    weekday: 'long', day: '2-digit', month: 'long',
+    hour: '2-digit', minute: 'numeric', hour12: true
+  }
+}
+
 export default {
   'en-us': {
-    day: {weekday: 'long',},
-    month: {month: 'long'},
-    year: {year: 'numeric'},
-    small: {
-      month: 'long', day: '2-digit'
-    },
-    short: {
-      year: 'numeric', month: 'long', day: '2-digit'
-    },
-    long: {
-      year: 'numeric', month: 'long', day: '2-digit',
-      hour: 'numeric', minute: 'numeric', hour12: true
-    },
-    explicit: {
-      weekday: 'long', year: 'numeric', month: 'long', day: '2-digit'
-    },
-    time: {
-      hour: '2-digit', minute: 'numeric', hour12: true
-    },
-    shortHuman: {
-      weekday: 'long', day: '2-digit', month: 'long',
-      hour: '2-digit', minute: 'numeric', hour12: true
-    }
+    ...en
+  },
+  'en' : {
+    ...en
   },
   'es': {
     day: {weekday: 'long',},
