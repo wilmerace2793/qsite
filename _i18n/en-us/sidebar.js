6114aad5d1e6168745a6530de2365dd335ca799b
Henry Rodriguez <139818140+hrodriguezimagina@users.noreply.github.com>
2024-06-27 11:47:26 -0500

    Modules translations (#456)
    * - removed and replaced by src/setup/translations
    
    * + moved ichat translations to qchat
    
    * + added translations
    
    * - removed i18n.json file

--- _i18n/en-us/sidebar.js
+++
@@ -1,9 +0,0 @@
-export default {
-  adminGroup: 'Site',
-  adminIndex: 'Settings',
-  adminDynamicForm: 'Dynamic Form',
-  adminOrganization: 'Organizations List',
-  adminCategories : 'Categories',
-  adminIcruds: 'Icruds',
-  adminOrganizationForm: 'Form',
-}
--- _i18n/es/sidebar.js
+++
@@ -1,9 +0,0 @@
-export default {
-  adminGroup: 'Sitio',
-  adminIndex: 'Ajustes',
-  adminDynamicForm: 'Formulario Dinámico',
-  adminOrganization: 'Lista de Organizaciónes',
-  adminCategories: 'Categorias',
-  adminIcruds: 'Icruds',
-  adminOrganizationForm: 'Formulario',
-}
